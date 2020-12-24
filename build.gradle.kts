7d2a782e308fc80c689275d961024f6da9e519fd
Florentine Dömges <florentine.doemges@grandcentrix>
2020-12-24 07:35:17 +0100

    fixed

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,8 @@ plugins {
 
 group = "net.doemges"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_15
+java.sourceCompatibility = JavaVersion.VERSION_11
+java.targetCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
